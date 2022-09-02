fde9280a97e477d97ab9fde4c03901f8d2a10a17
Mesbah-Himel <mesbah0010@gmail.com>
2022-09-02 15:12:23 +0600

    make it better

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const todos = require("./data/todos");
 const videos = require("./data/videos");
 const videos2 = require("./data/videos2");
 const transactions = require("./data/transactions");
+const tags = require("./data/tags.json");
 
 module.exports = {
   todos,
